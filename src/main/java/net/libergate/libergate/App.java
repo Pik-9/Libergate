cea8a129755009aefd8055d7fc9a14c809bc3a0f
Daniel Steinhauer <d.steinhauer@mailbox.org>
2020-05-23 23:28:03 +0200

    Initial app structure with maven pom.xml.

---
+++ src/main/java/net/libergate/libergate/App.java
@@ -0,0 +1,13 @@
+package net.libergate.libergate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
